b5092cc4da93f781c146e36a6d41df142062e060
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-25 22:51:34 -0700

    solution found

--- Solution.java
+++ Solution.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+
 public class Solution {
     public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && map.get(nums[i]) >= nums.length / 2) {
+                return nums[i];
+            } else {
+                int val = !map.containsKey(nums[i]) ? 1 : map.get(nums[i]) + 1;
+                map.put(nums[i], val);
+            }
+        }
+        
         return 0;
     }
 }
