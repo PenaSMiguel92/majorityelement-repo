ff77c23690e3e84c0fd878a64ea13711c2f6b6d5
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-25 22:56:45 -0700

    handle cases where a single element exists

--- Solution.java
+++ Solution.java
@@ -2,6 +2,8 @@ import java.util.HashMap;
 
 public class Solution {
     public int majorityElement(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i]) && map.get(nums[i]) >= nums.length / 2) {
