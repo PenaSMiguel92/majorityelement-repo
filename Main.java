fdc488a2e6d877cdb8415fa624395494894524a0
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-25 22:41:55 -0700

    main method impl

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Solution sol = new Solution();
+
+        System.out.println(sol.majorityElement(new int[] { 3, 2, 3 }));
+        System.out.println(sol.majorityElement(new int[] {2, 2, 1, 1, 1, 2, 2}));
     }
 }
